98001967561e7cc2912df1b1228217cb00929e09
MaAlexandre <masse.alexandre@hotmail.fr>
2015-04-01 13:44:21 +0200

    début script supp

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-//Quand on va cliquer sur le bouton de connexion
-$("#co").on("click",function(){
-    $.ajax({
-        type:"POST"
 
-    });
+$("form").attr("action","#");
+
+//Quand on va cliquer sur le bouton de connexion
+$("#co").on("click",function(){
+    $.ajax({
+        type:"POST"
+
+    });
 }
+
+$("#deco").on("click", function(){});
