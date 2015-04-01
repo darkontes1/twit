ed9c44f0513e7c96bba3f6774e77c15ad4dd0150
darkontes1 <darkontes@hotmail.fr>
2015-04-01 13:46:59 +0200

    script

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-
 $("form").attr("action","#");
 
-//Quand on va cliquer sur le bouton de connexion
-$("#co").on("click",function(){
-    $.ajax({
-        type:"POST"
-
-    });
-}
+//Quand on va cliquer sur le bouton de connexion
+$("#co").on("click",function(){
+    $.ajax({
+        type:"POST"
 
-$("#deco").on("click", function(){});
+
+    });
+});
+
+$("#deco").on("click", function(){
+    $.ajax({
+        type:"POST"
+
+
+    });
+});
