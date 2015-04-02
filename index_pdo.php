9b8ae7282bb6ddd7289e8221deede88e788aa482
darkontes1 <darkontes@hotmail.fr>
2015-04-02 10:22:40 +0200

    tata

--- index_pdo.php
+++ index_pdo.php
@@ -263,37 +263,6 @@ session_start();
                     header('location: index_pdo.php');
                 }
                 else{
-<<<<<<< HEAD
-                    for($i=0;$i<$taille;$i++){
-                        if($_SESSION['login']==$result[$i]['loginUser']){
-                            echo '<article>';
-                            echo '<div id="top-article">';
-                            echo '<p><b>'.date('j-m-y',strtotime($result[$i]['dateTwit'])).'</b>';
-                            echo '<br/>'.date('H:i:s',strtotime($result[$i]['dateTwit'])).'</p>';
-                            echo '</div>';
-                            echo '<p>'.$result[$i]['messageTwit'].'...<br/>@'.$result[$i]['loginUser'].'</p>';
-                            //IMPORTANT !!! syntaxe d'un get à la place de faire un form pour une action
-                            echo '<a class="bouton-action" href="index_pdo.php?action=modifier&idTwit='.$result[$i]['idTwit'].'">modifier</a>';
-                            echo '<a class="bouton-action" href="index_pdo.php?action=supprimer&idTwit='.$result[$i]['idTwit'].'">supprimer</a>';
-                            echo '</article>';
-                        }
-                        else{
-                            echo '<article>';
-                            echo '<div id="top-article">';
-                            echo '<p><b>'.date('j-m-y',strtotime($result[$i]['dateTwit'])).'</b>';
-                            echo '<br/>'.date('H:i:s',strtotime($result[$i]['dateTwit'])).'</p>';
-                            echo '</div>';
-                            echo '<p>'.$result[$i]['messageTwit'].'...<br/>@'.$result[$i]['loginUser'].'</p>';
-                            //IMPORTANT !!! syntaxe d'un get à la place de faire un form pour une action
-                            echo '<a class="bouton-action" href="index_pdo.php?action=retwit&idTwit='.$result[$i]['idTwit'].'">retwit</a>';
-                            //Si il est favori
-                            $query = 'SELECT * FROM favori WHERE idUser = "'.$_SESSION['id'].'" AND idTwit = "'.$result[$i]['idTwit'].'"';
-                            $data = $db->prepare($query);
-                            $data->execute();
-                            $result2 = $data->fetchAll(PDO::FETCH_ASSOC);
-                            if(count($result2)>0){
-                                echo '<a class="fav-orange" class="bouton-action" href="index_pdo.php?action=favori&idTwit='.$result[$i]['idTwit'].'">favori</a>';
-=======
                     if(isset($_POST['javascript']) && $_POST['javascript']=="oui" ){    
                         for($i=0;$i<$taille;$i++){
                             if($_SESSION['login']==$result[$i]['loginUser']){
@@ -345,7 +314,6 @@ session_start();
                                 echo '<a class="bouton-action" href="index_pdo.php?action=modifier&idTwit='.$result[$i]['idTwit'].'">modifier</a>';
                                 echo '<a class="bouton-action" href="index_pdo.php?action=supprimer&idTwit='.$result[$i]['idTwit'].'">supprimer</a>';
                                 echo '</article>';
->>>>>>> origin/master
                             }
                             else{
                                 echo '<a class="bouton-action" href="index_pdo.php?action=favori&idTwit='.$result[$i]['idTwit'].'">favori</a>';
