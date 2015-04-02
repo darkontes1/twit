034d5189dfcfd388ebe331b6f698f758459b44b0
darkontes1 <darkontes@hotmail.fr>
2015-04-02 11:14:00 +0200

    tata

--- index_pdo.php
+++ index_pdo.php
@@ -63,6 +63,23 @@ session_start();
         }
     }
 
+    //Bouton pour ajouter un twit appuyé
+    if(isset($_POST['ajout'])){
+            $idU=$_SESSION['id'];
+            $twit = $_POST['ajouttweet'];
+            $ReqTwit = 'INSERT INTO twit (messageTwit) VALUES (:message)';
+            $oTwit = $db->prepare($ReqTwit);
+            $oTwit->bindValue('message', $twit);
+            $oTwit->execute();
+            
+            $query = 'INSERT INTO reltwitusers(idUser,idTwit) VALUES (:idU,:idT)';
+            $tab = array('idU'=>$idU,
+                'idT'=>$db->lastInsertId());
+            $data = $db->prepare($query);
+            $data->execute($tab);
+                    
+        }
+
     //Bouton de déco est appuyé
     /*if(isset($_POST['deco'])){
         $_SESSION['connect'] = FALSE;
